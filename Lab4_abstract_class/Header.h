09514242bf5c314881ed36029e36f661a73c01e1
NikitaMon <Nikon30050@mail.ru>
2023-04-25 05:45:59 +0300

    lab4 try 1

---
+++ Lab4_abstract_class/Header.h
@@ -0,0 +1,125 @@
+#pragma once
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iostream>
+
+
+using namespace std;
+
+
+
+class Figure
+{
+public:
+
+	//конструктор по умолчанию
+	Figure() {
+
+	}
+
+	//конструктор с присваиванием параметров двух сторон
+	Figure(double x1, double y1) {
+
+	}
+
+	Figure(double x1)
+	{
+
+	}
+
+	//виртуальные функции
+	virtual double area() = 0;
+	virtual double perimeter() = 0;
+
+	//гетеры
+	virtual double getX() const = 0;
+	virtual double getY() const = 0;
+	virtual double getZ() const = 0;
+	virtual string getName() const = 0;
+
+	//сетеры
+	virtual void setX(double x1) = 0;
+	virtual void setY(double y1) = 0;
+	virtual void setZ(double z1) = 0;
+	virtual void setName(string name1) = 0;
+};
+
+
+/*
+*	класс треугольника
+*/
+class Triangle :public Figure
+{
+private:
+	double x;
+	double y;
+	double z;
+	string name;
+
+public:
+	Triangle() :Figure()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+		setName("Неизветно");
+	}
+
+	Triangle(double x1, double y1) :Figure(x1, y1)
+	{
+		setX(x1);
+		setY(y1);
+		setName("Треугольник");
+		setZ((sqrt(x1 * x1 + y1 * y1 - 2 * x1 * y1 * cos(90 * M_PI / 180))));
+	}
+
+	Triangle(double x1) :Figure(x1)
+	{
+		setX(x1);
+		setY(x1);
+		setName("Треугольник две стороны равны");
+		setZ((sqrt(x1 * x1 + getY() * getY() - 2 * x1 * getY() * cos(90 * M_PI / 180))));
+	}
+
+	//виртуальные функции
+	double area()override {
+		return getX() * getY() / 2;
+	}
+
+	double perimeter()override {
+		return getX() + getY() + getZ();
+	}
+
+	//гетеры
+	double getX() const override { return x; };
+	double getY() const override { return y; };
+	double getZ() const override { return z; };
+	string getName() const override { return name; };
+
+	//сетеры
+	void setX(double x1)override {
+		if (x1 <= 0)
+			throw invalid_argument{ "Сторона X не может быть равна или меньше 0" };//обработка исключения
+		else
+			x = x1;
+	}
+
+	void setY(double y1)override {
+		if (y1 <= 0)
+			throw invalid_argument{ "Сторона Y не может быть равна или меньше 0" };
+		else
+			y = y1;
+	}
+
+	void setZ(double z1)override {
+		if (z1 <= 0)
+			throw invalid_argument{ "Сторона Z не может быть равна или меньше 0" };
+		else
+			z = z1;
+	}
+
+	void setName(string name1)override {
+		name = name1;
+	}
+
+};
---
+++ Lab4_abstract_class/Lab4_abstract_class.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+Figure:Triangle a[5];
+
+	try {
+		a[0] = Triangle(3, 4);
+	}
+	catch (invalid_argument& e) {
+		cout << "a[0]" << endl;
+		cout << e.what() << endl;
+	}
+
+	try {
+		a[1] = Triangle(0);
+	}
+	catch (invalid_argument& e) {
+		cout << "a[1]" << endl;
+		cout << e.what() << endl;
+	}
+
+	try {
+		a[2] = Triangle(0, 3);
+	}
+	catch (invalid_argument& e) {
+		cout << "a[2]" << endl;
+		cout << e.what() << endl;
+	}
+
+	try {
+		a[3] = Triangle(5);
+	}
+	catch (invalid_argument& e) {
+		cout << "a[3]" << endl;
+		cout << e.what() << endl;
+	}
+
+	try {
+		a[4] = Triangle(5, -4);
+	}
+	catch (invalid_argument& e) {
+		cout << "a[4]" << endl;
+		cout << e.what() << endl;
+	}
+	cout << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Номер " << i << endl;
+		cout << a[i].getName() << endl;
+		cout << "Стороны равны " << a[i].getX() << ", " << a[i].getY() << ", " << a[i].getZ() << endl;
+		cout << "Площадь равна " << a[i].area() << endl;
+		cout << "Периметр равен " << a[i].perimeter() << endl << endl;
+
+	}
+
+	return 0;
+}
