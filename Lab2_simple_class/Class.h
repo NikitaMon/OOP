c997bd8753ea7c3dcf100239bf7dfe36d2b39230
NikitaMon <Nikon30050@mail.ru>
2023-05-05 03:34:20 +0300

    ver 1

---
+++ Lab2_simple_class/Class.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <iostream>
+using namespace std;
+/*
+*  ласс Triangle
+* предназначен дл€ получени€ третьей стороны треугольника, площади и периметра
+* по двум сторонам и углу между ними 90 градусов
+* 
+* раньше класс называлс€ Figure по причине того что было 3 функции,
+* дл€ треугольника, пр€моугольника и круга
+* по поправкам преподавател€ ныне удалены и класс переименован
+*/
+class Triangle
+{
+private:
+	double x;	//перва€ сторона
+	double y;	//втора€ сторона
+	double z;	//треть€ сторона
+	string name;	//название фигуры
+
+public:
+
+	//конструкторы
+	Triangle();
+	Triangle(double x1, double y1);
+
+	//функци€ площади
+	double Area();
+
+	//функци€ периметра
+	double Perimetr();
+
+	//гетеры
+	//если значение возвращаемое гетером слишком большое, 
+	//дл€ оптимизации программы возвращают не значение переменной а адрес
+	//и чтобы случайно не изменить значение приватной переменной с помощью гетера добавл€ют const
+	//что запретит подобные махинации
+	const double getX() { return x; }
+	const double getY() { return y; }
+	const double getZ() { return z; }
+	const string getName() { return name; }
+	
+	//пример плохого гетера
+	double &getXadr() { return x; }
+
+	//protected чтобы не вызывались в main
+protected:
+	//сетеры
+	void setX(double x1) {
+		if (x1 <= 0)
+			throw invalid_argument{ "—торона X не может быть равна или меньше 0" };//обработка исключени€
+		else
+			x = x1;
+	}
+
+	void setY(double y1) {
+		if (y1 <= 0)
+			throw invalid_argument{ "—торона Y не может быть равна или меньше 0" };
+		else
+			y = y1;
+	}
+};
--- Lab2_simple_class/Lab2_simple_class.cpp
+++ Lab2_simple_class/Lab2_simple_class.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-#include "Header.h"
+#include "Class.h"
 
 using namespace std;
 
@@ -7,22 +7,31 @@ int main()
 {
 	setlocale(LC_ALL, "russian");
 
-	Figure a[1];
-
+	Triangle a[1];
+	
+	
 	try//проверка значений
 	{
-		a[0].triangle(3, 4);
+		a[0] = Triangle(3, 4);
 	}
 	catch (invalid_argument& e)
 	{
 		cout << e.what() << endl;
 	}
-
+	
 	cout << a[0].getName() << endl;
 	cout << "Стороны равны " << a[0].getX() << ", " << a[0].getY() << ", " << a[0].getZ() << endl;
-	cout << "Площадь равна " << a[0].getArea() << endl;
-	cout << "Периметр равен " << a[0].getPerimetr() << endl << endl;
+	cout << "Площадь равна " << a[0].Area() << endl;
+	cout << "Периметр равен " << a[0].Perimetr() << endl << endl;
 
+	//продожение примера плохого гетера
+	//и таким образом можно изменить значение приватной переменной с помощью гетера 
+	//но если бы гетер возвращал константу то и изменение было бы невозможным
+	double *x = &a[0].getXadr();
+	*x = -100;
+	cout << x << endl;
+	cout << "Стороны равны " << a[0].getX() << ", " << a[0].getY() << ", " << a[0].getZ() << endl;
+	
 	return 0;
 }
 
